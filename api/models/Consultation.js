2f1659d793775d482af9d7f4a04b788ab41c23d4
rmidha2 <rmidha2@uwo.ca>
2017-06-03 15:03:45 -0400

    Update Consultation.js

--- api/models/Consultation.js
+++ api/models/Consultation.js
@@ -6,7 +6,7 @@ var ConsultationSchema = new Schema({
 		first: String,
 		last: String,
 		email: 	{type: String,  
-				unique: true},
+			unique: true},
 		grade: String,
 		school: String,
 		answers: Array,
